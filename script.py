58726a0742ffb4561f7f728c5dec021373913051
Tesfa Yohannes <tmyohannes@gmail.com>
2021-05-18 10:05:17 +0800

    Somewhat useful

---
+++ script.py
@@ -0,0 +1,10 @@
+# Ok. First base assumption. 
+# we would operate on simliar styles of drawing. 
+
+# Take a primitive shape that exists in both forms. 
+# Subsititute a shape of that shape from one picture and replace it from another one. 
+# Then continue on it. 
+# Until it stops. 
+
+
+# Jupyter notebook
